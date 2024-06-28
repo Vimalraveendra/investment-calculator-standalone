7406461738d775376ab737a338c9f6adf3a9d302
Vimal Raveendran <vimalraveendra@gmail.com>
2024-06-28 20:11:18 +0200

    added two way data binding using formsModule

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-
+  @Input()initialInvestment='0';
+  @Input()annualInvestment='0';
+  @Input()expectedReturn='5';
 }
